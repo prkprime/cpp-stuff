e94db07a9254387d999b2d48b31d8aaec7f524c3
Pratik Gorade <pratikgorade0@gmail.com>
2020-09-19 16:28:09 +0530

    created program that takes input of basic types and prints to stdout also verified if input value was correct type
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

---
+++ datatypes.cpp
@@ -0,0 +1,53 @@
+// datatypes.cpp
+// Author : Pratik Gorade
+
+#include <iostream>
+#include <limits>
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+    float f;
+    char c;
+
+    cout << "Enter any integer : ";
+    while (!(cin >> n))
+    {
+        cout << "Bad Value. Try Again : ";
+        cin.clear();                                         // clears the error flag on cin so that future I/O will not be affected
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // clears the input buffer till newline to avoid parsing errors
+        // cin.ignore(arg1, arg2), arg 1 -> characters to skip (can give integer), arg2 -> delimeter after which cin stops ignoring
+        // numeric_limits<streamsize>::max() -> it skips the upper limit of the size of the stream
+    }
+    cout << "You entered the number : " << n << endl;
+
+    cout << "Enter any string : ";
+    while (!(cin >> s))
+    {
+        cout << "Bad Value. Try Again : ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    cout << "You Entered the string : " << s << endl;
+
+    cout << "Enter any float value : ";
+    while (!(cin >> f))
+    {
+        cout << "Bad Value. Try Again : ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    cout << "You Entered the float : " << f << endl;
+
+    cout << "Enter any character : ";
+    while (!(cin >> c))
+    {
+        cout << "Bad Value. Try AGain : ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    cout << "You Entered the character : " << c << endl;
+
+    return 0;
+}
